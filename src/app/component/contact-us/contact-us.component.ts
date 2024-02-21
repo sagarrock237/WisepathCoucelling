0e8946c41d2d892e9dfd5f2f13122148994783f4
sagarrock237 <138840815+sagarrock237@users.noreply.github.com>
2024-02-21 11:54:48 +0530

    Changes

--- src/app/component/contact-us/contact-us.component.ts
+++ src/app/component/contact-us/contact-us.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject, TemplateRef  } from '@angular/core';
+import { ModalDismissReasons, NgbDatepickerModule, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-contact-us',
@@ -6,5 +7,45 @@ import { Component } from '@angular/core';
   styleUrls: ['./contact-us.component.css']
 })
 export class ContactUsComponent {
+
+  private modalService = inject(NgbModal);
+	closeResult = '';
+
+
+  // OPEN ADD A TICKET FORM
+  open(content: TemplateRef<any>) {
+		this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title' }).result.then(
+			(result) => {
+				this.closeResult = `Closed with: ${result}`;
+			},
+			(reason) => {
+				this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+			},
+		);
+	}
+
+  private getDismissReason(reason: any): string {
+		switch (reason) {
+			case ModalDismissReasons.ESC:
+				return 'by pressing ESC';
+			case ModalDismissReasons.BACKDROP_CLICK:
+				return 'by clicking on a backdrop';
+			default:
+				return `with: ${reason}`;
+		}
+  }
+
+// SUBMIT TICKET POPUP
+  SubmitTicketPopup(ticketPopup: TemplateRef<any>) {
+		this.modalService.open(ticketPopup, { ariaLabelledBy: 'modal-basic-title' }).result.then(
+			(result) => {
+				this.closeResult = `Closed with: ${result}`;
+			},
+			(reason) => {
+				this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+			},
+		);
+	}
+
 
 }
